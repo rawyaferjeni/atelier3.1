cecaadfd90488a71515811634d4fa1056da501cb
rawyaferjeni <98555402+rawyaferjeni@users.noreply.github.com>
2022-12-22 22:46:36 +0100

    atelier3-ch7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ConvertisseurComponentComponent } from './convertisseur-component/convertisseur-component.component';
+import { ToEuroConvertComponentComponent } from './to-euro-convert-component/to-euro-convert-component.component';
+import { ActorComponentComponent } from './actor-component/actor-component.component';
+import { ActorsComponentComponent } from './actors-component/actors-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent
+    TemplateDrivenFormComponent,
+    ConvertisseurComponentComponent,
+    ToEuroConvertComponentComponent,
+    ActorComponentComponent,
+    ActorsComponentComponent,
+
   ],
   imports: [
     BrowserModule,
